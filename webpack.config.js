d6a1cf11fef2e42b6204810d9d93478c87d69dff
luobin <1096422441@qq.com>
2019-09-29 10:42:33 +0800

    webpack配置

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // 将 JS 字符串生成为 style 节点
+                    "css-loader", // 将 CSS 转化成 CommonJS 模块
+                    "sass-loader" // 将 Sass 编译成 CSS，默认使用 Node Sass
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.jsx?$/,
+                use: ['babel-loader'],
+            }
+        ]
+    }
+};
